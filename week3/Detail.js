915bbdff1137e993c2e8d7341a5ed6dde5a4218c
LEESUNGMIN <85213997+tjdlas2412@users.noreply.github.com>
2021-09-26 22:48:48 +0900

    Quiz

---
+++ week3/Detail.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {useParams, useHistory} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
+import {deleteBucket, updateBucket} from "./redux/modules/bucket";
+
+const Detail = (props) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const params = useParams();
+    const bucket_index = params.index;
+    const bucket_list = useSelector((state) => state.bucket.list);
+
+    return (
+        <div>
+            <h1>{bucket_list[bucket_index].text}</h1>
+            <button onClick={() => {
+                dispatch(updateBucket(bucket_index));
+            }}>완료하기</button>
+            <button
+                onClick={() => {
+                    console.log("삭제하기 버튼을 눌렀어!");
+                    dispatch(deleteBucket(bucket_index));
+                    history.goBack();
+                }}>삭제하기</button>
+        </div>
+    );
+}
+
+export default Detail;
