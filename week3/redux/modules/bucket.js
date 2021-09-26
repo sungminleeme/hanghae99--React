b9a91ddb33cfcf9c5329e7dfbce368b105d431e1
LEESUNGMIN <85213997+tjdlas2412@users.noreply.github.com>
2021-09-26 22:50:25 +0900

    Add files via upload

---
+++ week3/redux/modules/bucket.js
@@ -0,0 +1,77 @@
+// bucket.js Actions
+const CREATE = "bucket/CREATE";
+const UPDATE = "bucket/UPDATE";
+const DELETE = "bucket/DELETE";
+
+const initialState = {
+    list: [
+        {
+            text: "영화관 가기",
+            completed: false
+        }, {
+            text: "매일 책읽기",
+            completed: false
+        }, {
+            text: "수영 배우기",
+            completed: false
+        }, {
+            text: "코딩하기",
+            completed: false
+        }
+    ]
+};
+
+// Action Creators
+export function createBucket(bucket) {
+    console.log("액션을 생성할거야!");
+    return {type: CREATE, bucket: bucket};
+}
+
+export function updateBucket(bucket_index){
+    return {type: UPDATE, bucket_index};
+}
+
+export function deleteBucket(bucket_index) {
+    console.log("지울 버킷 인덱스", bucket_index);
+    return {type: DELETE, bucket_index};
+}
+
+// Reducer
+export default function reducer(state = initialState, action = {}) {
+    switch (action.type) {
+        case "bucket/CREATE":
+            {
+                console.log("이제 값을 바꿀거야!");
+                const new_bucket_list = [
+                    ...state.list,
+                    action.bucket
+                ];
+                return {list: new_bucket_list};
+            }
+
+        case "bucket/UPDATE": {
+            const new_bucket_list = state.list.map((l, idx) => {
+                
+                if(parseInt(action.bucket_index) === idx){
+                    return{...l, completed: true};
+                }else{
+                    return l;
+                }
+            })
+            return {list: new_bucket_list};
+        }    
+
+        case "bucket/DELETE":
+            {
+                const new_bucket_list = state
+                    .list
+                    .filter((l, idx) => {
+                        return parseInt(action.bucket_index) !== idx;
+                    });
+
+                return {list: new_bucket_list};
+            }
+        default:
+            return state;
+    }
+}
